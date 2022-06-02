d56672ae82f3f070d443bcec79dc87e2b8fdc8ce
Kingsize072 <mr.purit@gmail.com>
2022-06-02 22:51:38 +0100

    Do a test

--- ConvertData.py
+++ ConvertData.py
@@ -1,4 +1,4 @@
-a = "dfsdf"
+a = "1234123"
 b = 150
 c = float(a) + b
 print(c)
---
+++ InputData.py
@@ -0,0 +1,19 @@
+'''print("--------------------------")
+print("  Welcome to Purit Shop")
+print("--------------------------")
+print("กรุณากรอกชื่อของคุณครับ")
+x = input()
+print("สวัสดีครับ คุณ", x)'''
+'''
+print("--------------------------")
+print("  Welcome to Purit Shop")
+print("--------------------------")
+name = input("กรุณากรอกชื่อของคุณครับ : ")
+lastName = input("กรุณากรอกนามสกุลของคุณครับ : ")
+print("สวัสดีครับ คุณ", name, lastName)'''
+
+x = int(input("Number 1 : "))
+y = int(input("Number 2 : "))
+result = x + y
+print("Your result from", x, "+", y, "is", result)
+
